c871e6716278e69f7922c588fc49a25402809ae0
paperbenni <paperbenni@gmail.com>
2021-09-15 15:15:58 +0200

    init instantpass.sh

---
+++ instantpass.sh
@@ -0,0 +1,115 @@
+#!/usr/bin/env bash
+
+# password manager using pass and instantmenu
+
+shopt -s nullglob globstar
+
+selectpassword() {
+    printf '%s\n' "${password_files[@]}" | smartcat instantpass | instantmenu -q 'password selection' -l 20 -rc "$0 --menu"
+}
+
+prefix=${PASSWORD_STORE_DIR-~/.password-store}
+
+if ! [ -e "$prefix" ]; then
+    notify-send 'no password store found, please run pass init in a terminal'
+    exit 1
+fi
+
+password_files=("$prefix"/**/*.gpg)
+password_files=("${password_files[@]#"$prefix"/}")
+password_files=("${password_files[@]%.gpg}")
+
+insertpw() {
+    if [ -z "$1" ]; then
+        PWNAME="$(imenu -i 'insert password' 'password name')"
+        [ -z "$PWNAME" ] && return 1
+    else
+        PWNAME="$1"
+    fi
+
+    PWCONTENT="$(imenu -P 'leave empty to generate random password' || echo 'quitplease')"
+    [ "$PWCONTENT" = "quitplease" ] && return 1
+
+    if [ -z "$PWCONTENT" ]; then
+        pass generate "$PWNAME" 15
+        notify-send "generated 15 character password for $PWNAME"
+        return
+    else
+        CHECKPW="$(imenu -P 'confirm password' || echo 'quitplease')"
+        [ "$CHECKPW" = "quitplease" ] && return 1
+        if ! [ "$CHECKPW" = "$PWCONTENT" ]; then
+            imenu -e 'passwords do not match'
+            unset PWCONTENT
+            unset CHECKPW
+            insertpw "$PWNAME"
+            return
+        else
+            {
+                echo "$PWCONTENT"
+                echo "$PWCONTENT"
+            } | pass insert "$PWNAME"
+            return
+        fi
+
+    fi
+}
+
+cleanpasswords() {
+    refreshpasswords
+    echo 'cleaning cache from removed password'
+    printf '%s\n' "${password_files[@]}" | smartcat --clean instantpass
+}
+
+refreshpasswords() {
+    password_files=("$prefix"/**/*.gpg)
+    password_files=("${password_files[@]#"$prefix"/}")
+    password_files=("${password_files[@]%.gpg}")
+}
+
+deletepw() {
+    DELETEPASS="$(selectpassword)"
+    [ -z "$DELETEPASS" ] && return 1
+    cleanpasswords
+    pass rm "$DELETEPASS" || {
+        imenu -e 'failed to delete password'
+        return 1
+    }
+}
+
+if [ "$1" = '--menu' ]; then
+    CHOICE="$(
+        echo ':gadd password
+:rdelete password
+close menu' | instantmenu -l 20 -rc "$0"
+    )"
+    echo choice "$CHOICE"
+    [ -z "$CHOICE" ] && exit
+    case "$CHOICE" in
+    *delete*)
+        deletepw
+        ;;
+    *add*)
+        insertpw
+        ;;
+    *)
+        exit
+        ;;
+    esac
+
+    # insertpw
+    exit
+fi
+
+password="$(selectpassword)"
+
+[[ -n $password ]] || exit
+
+if grep -q '\.otp$' <<<"$password"; then
+    pass otp -c "$password" 2>/dev/null
+    imenu -t 'copied one time password to clipboard'
+else
+    pass show -c "$password" 2>/dev/null
+    imenu -t 'copied password to clipboard'
+fi
+
+smartcat instantpass "$password" 1000 &
